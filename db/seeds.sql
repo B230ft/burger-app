5664ac55a4f8a122cf573ca764a4a2d03d485f31
B230ft <igorniyazov@gmail.com>
2019-01-28 17:46:13 -0800

    added final files for app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Creating seed data for the burgers_db database --
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMAc', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
